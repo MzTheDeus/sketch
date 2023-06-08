e23845a9c3e3c7b36789a91b4ee65da1ca6484df
MzTheDeus <bozzaruh@gmail.com>
2023-06-08 23:17:44 +0200

    v0.2

--- scripty.js
+++ scripty.js
@@ -1,26 +1,30 @@
 
-
-
-
 function testFunc() {
-  let n = prompt('type Number 1 - 20')
-  for (let i = 0; i < n; i++) {
-    let div1 = document.createElement('div');
-    div1.classList.add('mini');
-    for(let j = 0; j < n; j++) {
-      let div = document.createElement('div');
-      div.classList.add('helo');
-      div1.appendChild(div);
-    }
-    document.getElementById('container').appendChild(div1)
-    console.log(div1)
-  }
-  
-  const div = document.querySelectorAll('.helo');
-  div.forEach((item) => {
-    item.addEventListener('mouseover',(e) => {
-      e.target.classList.add('change');
-    });
+  let one = document.querySelectorAll('.mini');
+  one.forEach((item) => {
+    item.remove();
   });
+  
+  let n = prompt('Type Number 1 - 50')
+  while (n > 50 || n <= 0)  {
+    n = prompt('Try again')
+  }
+    for (let i = 0; i < n; i++) {
+      let div1 = document.createElement('div');
+      div1.classList.add('mini');
+      for(let j = 0; j < n; j++) {
+        let div = document.createElement('div');
+        div.classList.add('helo');
+        div1.appendChild(div);
+      }
+      document.getElementById('container').appendChild(div1)
+    }
+    
+    const ballsack = document.querySelectorAll('.helo');
+    ballsack.forEach((item) => {
+      item.addEventListener('mouseover',(e) => {
+        e.target.classList.add('change');
+      });
+    });
   
 }
